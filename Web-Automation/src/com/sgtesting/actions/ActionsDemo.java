50869a90e49be4af4fda3995768f2f7f2be7436d
megharajgk <megharaj.dss@gmail.com>
2021-06-08 20:18:26 +0530

    Action

--- Web-Automation/src/com/sgtesting/actions/ActionsDemo.java
+++ Web-Automation/src/com/sgtesting/actions/ActionsDemo.java
@@ -1,10 +1,64 @@
 package com.sgtesting.actions;
 
-public class ActionsDemo {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
-	public static void main(String[] args) 
-	{
+public class ActionsDemo 
+{
+	public static WebDriver driver=null;
+	public static void main(String[] args) {
+		launchBrowser();
+		navigate();
+		mouseDemo();
 		
 	}
+	
+	static void launchBrowser()
+	{
+		String path=null;
+		try
+		{
+			path=System.getProperty("user.dir");
+			System.setProperty("webdriver.chrome.driver", path+"\\Library\\drivers\\chromedriver.exe");
+			driver=new ChromeDriver();
+			driver.manage().window().maximize();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
 
+	static void navigate()
+	{
+		try
+		{
+			driver.get("https://www.flipkart.com/");
+			Thread.sleep(5000);
+			driver.findElement(By.xpath("//button[text()='✕']")).click();
+			Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	static void mouseDemo()
+	{
+		try
+		{
+			WebElement oEle=driver.findElement(By.xpath("//img[@alt='Fashion']"));
+			Thread.sleep(4000);
+			Actions oMouse=new Actions(driver);
+			oMouse.moveToElement(oEle).build().perform();
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//div[@class='_3XS_gI']/a[3]")).click();
+			Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
